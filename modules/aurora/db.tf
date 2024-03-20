869ed6022a699b95ea7cebbcf5cb5ce11a6de560
karlagabriel <karla.gabriel@hashicorp.com>
2024-03-20 09:54:42 -0500

    feat: global aurora

--- modules/aurora/db.tf
+++ modules/aurora/db.tf
@@ -1,15 +1,68 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_rds_cluster" "postgresql" {
-  cluster_identifier      = var.cluster_identifier
-  engine                  = "aurora-postgresql"
-  availability_zones      = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  database_name           = "mydb"
-  master_username         = "foobar123"
-  master_password         = "barfoo123"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = var.skip_final_snapshot
+provider "aws" {
+  region = var.primary_region
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = var.secondary_region
+}
+
+resource "aws_rds_global_cluster" "example" {
+  global_cluster_identifier = "global-test"
+  engine                    = "aurora-postgresql"
+  engine_version            = "11.9"
+  database_name             = "example_db"
+}
+
+resource "aws_rds_cluster" "primary" {
+  engine                    = aws_rds_global_cluster.example.engine
+  engine_version            = aws_rds_global_cluster.example.engine_version
+  cluster_identifier        = "test-primary-cluster"
+  master_username           = "username"
+  master_password           = "somepass123"
+  database_name             = "example_db"
+  global_cluster_identifier = aws_rds_global_cluster.example.id
+  db_subnet_group_name      = "default"
+  skip_final_snapshot = var.skip_final_snapshot
+}
+
+resource "aws_rds_cluster_instance" "primary" {
+  engine               = aws_rds_global_cluster.example.engine
+  engine_version       = aws_rds_global_cluster.example.engine_version
+  identifier           = "test-primary-cluster-instance"
+  cluster_identifier   = aws_rds_cluster.primary.id
+  instance_class       = "db.r4.large"
+  db_subnet_group_name = "default"
+}
+
+resource "aws_rds_cluster" "secondary" {
+  provider                  = aws.secondary
+  engine                    = aws_rds_global_cluster.example.engine
+  engine_version            = aws_rds_global_cluster.example.engine_version
+  cluster_identifier        = "test-secondary-cluster"
+  global_cluster_identifier = aws_rds_global_cluster.example.id
+  skip_final_snapshot       = var.skip_final_snapshot
+
+  depends_on = [
+    aws_rds_cluster_instance.primary
+  ]
+}
+
+resource "aws_rds_cluster_instance" "secondary" {
+  provider             = aws.secondary
+  engine               = aws_rds_global_cluster.example.engine
+  engine_version       = aws_rds_global_cluster.example.engine_version
+  identifier           = "test-secondary-cluster-instance"
+  cluster_identifier   = aws_rds_cluster.secondary.id
+  instance_class       = "db.r4.large"
+  db_subnet_group_name = "default"
 }
--- modules/aurora/variables.tf
+++ modules/aurora/variables.tf
@@ -9,3 +9,31 @@ variable "cluster_identifier" {
 variable "skip_final_snapshot" {
   default = false
 }
+
+variable "global_cluster_identifier" {
+  default = "global-cluster"
+}
+
+variable "primary_cluster_identifier" {
+  default = "primary-cluster"
+}
+
+variable "primary_instance_identifier" {
+  default = "primary-instance"
+}
+
+variable "secondary_cluster_identifier" {
+  default = "secondary-cluster"
+}
+
+variable "secondary_instance_identifier" {
+  default = "secondary-instance"
+}
+
+variable "primary_region" {
+  default = "us-east-1"
+}
+
+variable "secondary_region" {
+  default = "us-west-2"
+}
